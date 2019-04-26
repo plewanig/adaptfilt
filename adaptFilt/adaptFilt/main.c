2a4b2c8026e10a37a4f98e6c8775bbdc9e6df09a
plewanig <d_pl@gmx.de>
2019-04-26 09:56:17 +0200

    added xcode project

---
+++ adaptFilt/adaptFilt/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  adaptFilt
+//
+//  Created by Daniel Plewanig on 26.04.19.
+//  Copyright © 2019 daniel plewe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
